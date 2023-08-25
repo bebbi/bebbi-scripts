1fbe3115a9b697aca64c407a056d0c36b882d137
Stefan <stefan@gigmade.com>
2023-08-25 12:52:56 +0200

    Slightly more meaningful self-test message. Add test script

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,4 +1,4 @@
-// skipping out on building actuall tests for this package right now, this test is a sanity check that tests are being found and ran by the scripts/test script.
-test('hello world', () => {
-  expect('hi!').toBeTruthy()
+// skipping out on building actual tests for this package right now, this test is a sanity check that tests are being found and ran by the scripts/test script.
+test('TODO: Tests', () => {
+  expect('Tests called!').toBeTruthy()
 })
