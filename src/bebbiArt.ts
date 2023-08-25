cf9c61bd40806098652596237f1fd61a33991a3c
Stefan <stefan@gigmade.com>
2023-08-25 08:45:23 +0200

    Make bebbi art smaller

--- src/bebbiArt.ts
+++ src/bebbiArt.ts
@@ -1,16 +1,9 @@
-// https://patorjk.com/software/taag/#p=display&f=Big&t=bebbi
-// then escape \ to \\
-export const bebbiArt = `
-  _          _     _     _                      _       _ 
- | |        | |   | |   (_)                    (_)     | |
- | |__   ___| |__ | |__  _   ___  ___  ___ _ __ _ _ __ | |_ ___ 
- | '_ \\ / _ \\ '_ \\| '_ \\| | /__/ / __|/ __| '__| | '_ \\| __/ __|
- | |_) |  __/ |_) | |_) | |      \\__ \\ (__| |  | | |_) | |_\\__ \\
- |_.__/ \\___|_.__/|_.__/|_|      |___/\\___|_|  |_| .__/ \\__|___/
-                                                 | |
-                                                 |_|
-`
+// https://patorjk.com/software/taag
+// Escape \, `.
+export const bebbiArt = `        |---.,---.|---.|---.o   ,---.,---.,---.o,---.|--- ,---.
+with ❤️   |   ||---'|   ||   ||---\`---.|    |    ||   ||    \`---.
+         \`---'\`---'\`---'\`---'\`   \`---'\`---'\`    \`|---'\`---'\`---'`
 
 export const signOff = () => {
-  console.log('\n', 'Thanks for using:', bebbiArt)
+  console.log('\n', bebbiArt)
 }
